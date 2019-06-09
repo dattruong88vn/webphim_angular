db5c3865327e965c7af7d7c7f01e628bb15e1d55
Thanh Dat <dattruong88vn@gmail.com>
2019-06-09 17:42:44 +0700

    home menu done

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
+import { HomeMenuComponent } from './home-menu/home-menu.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, HomeMenuComponent],
+  exports: [HomeComponent, HomeMenuComponent],
   imports: [
     CommonModule,
     HomeRoutingModule
