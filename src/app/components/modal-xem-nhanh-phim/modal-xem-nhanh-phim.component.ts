b7d336947a63b2655d18d5ce42f7ab36d4f0349f
Thanh Dat <dattruong88vn@gmail.com>
2019-06-14 15:46:29 +0700

    variable, mixin, video youtube

--- src/app/components/modal-xem-nhanh-phim/modal-xem-nhanh-phim.component.ts
+++ src/app/components/modal-xem-nhanh-phim/modal-xem-nhanh-phim.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { ShareStoreService } from 'src/app/core/shared/share-store.service';
+import { Component, OnInit} from '@angular/core';
+import { ShareStoreService } from 'src/app/core/shared/share-store.service';   // Nhận data từ store
+import { DomSanitizer } from '@angular/platform-browser';   // Fix lỗi unsafe
 
 @Component({
   selector: 'app-modal-xem-nhanh-phim',
@@ -10,14 +11,26 @@ export class ModalXemNhanhPhimComponent implements OnInit {
 
   chiTietPhimFromShareStoreService = {};
 
+  originalLink = 'https://www.youtube.com/embed/';
+  embedLink = '';
+
+  viTriCat = 32;
+
   constructor(
-    private shareStore: ShareStoreService
+    private shareStore: ShareStoreService,
+    private sanitizer: DomSanitizer
   ) { }
 
   ngOnInit() {
     this.shareStore.shareChiTietPhim.subscribe((data: any) => {
-      this.chiTietPhimFromShareStoreService = data;
+      this.chiTietPhimFromShareStoreService = data; // lấy chi tiết phim
+      this.originalLink = data.Trailer;  // lấy link trailer
     })
+    this.embedLink = '';
   }
 
+  xemTrailer() {
+     this.embedLink = 'https://www.youtube.com/embed/';
+     this.embedLink += this.originalLink.slice(32);
+  }
 }
